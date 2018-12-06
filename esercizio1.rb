71c1135a589bc34a6c7670beb4a1ab1f2629c78f
verteransss <45625679+verteransss@users.noreply.github.com>
2018-12-06 17:02:44 +0100

    Update esercizio1.rb

--- esercizio1.rb
+++ esercizio1.rb
@@ -1,4 +1,25 @@
-live_loop :flibble do
-  sample :bd_haus, rate: 1
-  sleep 0.5
-end 
+2.times do
+  guit
+end
+1.times do
+  boom
+end
+2.times do
+  guit
+  boom
+end
+
+define :guit do
+  with_fx :echo, mix: 0.5, phase: 1 do
+    sample :guit_em9, rate: 0.5
+    sample :guit_em9, rate: -1
+  end
+  sleep 8
+end
+
+define :boom do
+  with_fx :reverb, room: 1 do
+    sample :bd_boom, amp: 10, rate: 1
+  end
+  sleep 4
+end
