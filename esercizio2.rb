59f1227e984272c4212ee3b8eca47e04bc928b0a
verteransss <45625679+verteransss@users.noreply.github.com>
2018-12-12 15:54:27 +0100

    Create esercizio2.rb

---
+++ esercizio2.rb
@@ -0,0 +1,15 @@
+sample "/Users/liceos.satta/Desktop/kiorgia.wav"
+
+
+
+
+sleep 2.5
+sample "/Users/liceos.satta/Desktop/kiorgia3.wav"
+sleep 0.5
+sample "/Users/liceos.satta/Desktop/kiorgia2.wav"
+sleep 2.0
+sample "/Users/liceos.satta/Desktop/kiorgia3.wav"
+sleep 0.5
+sample "/Users/liceos.satta/Desktop/kiorgia2.wav"
+
+
