eb00743a1a20ecbf6d437929158333895dc6ffb4
verteransss <45625679+verteransss@users.noreply.github.com>
2018-12-12 15:19:07 +0100

    Update esercizio1.rb

--- esercizio1.rb
+++ esercizio1.rb
@@ -1,15 +1,4 @@
 2.times do
-  guit
-end
-1.times do
-  boom
-end
-2.times do
-  guit
-  boom
-end
-
-define :guit do
   with_fx :echo, mix: 0.5, phase: 1 do
     sample :guit_em9, rate: 0.5
     sample :guit_em9, rate: -1
@@ -17,7 +6,22 @@ define :guit do
   sleep 8
 end
 
-define :boom do
+1.times do
+  with_fx :reverb, room: 1 do
+    sample :bd_boom, amp: 10, rate: 1
+  end
+  sleep 4
+end
+
+2.times do
+  with_fx :reverb, room: 1 do
+    sample :bd_boom, amp: 10, rate: 1
+  end
+  with_fx :echo, mix: 0.5, phase: 1 do
+    sample :guit_em9, rate: 0.5
+    sample :guit_em9, rate: -1
+  end
+  sleep 4
   with_fx :reverb, room: 1 do
     sample :bd_boom, amp: 10, rate: 1
   end
